b35f04130b5797037dd9ef0ee3961563dff58816
Stephanie King <stepking@amazon.com>
2015-03-03 10:40:15 -0800

    wrapper v1

--- Berksfile
+++ Berksfile
@@ -1,3 +1,5 @@
 source "https://supermarket.getchef.com"
 
 metadata
+
+cookbook "stepking_remotefile", github: "stepking/stepking_remotefile"
--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,4 @@ description      'Installs/Configures stepking_wrapper'
 long_description 'Installs/Configures stepking_wrapper'
 version          '0.1.0'
 
+depends "stepking_remotefile"
