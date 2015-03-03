c51d7ce6b8ffc1ae44150b48662f4ec26b1590fd
Stephanie King <stepking@amazon.com>
2015-03-03 10:34:15 -0800

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 0.1.0
+
+Initial release of stepking_wrapper
+
+* Enhancements
+  * an enhancement
+
+* Bug Fixes
+  * a bug fix
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'stepking_wrapper'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures stepking_wrapper'
+long_description 'Installs/Configures stepking_wrapper'
+version          '0.1.0'
+
